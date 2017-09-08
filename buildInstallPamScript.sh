f946740414e30a95fbaa3fbbd36e35eb52bfd926
mtekman <m.tekman@ucl.ac.uk>
2017-09-08 17:03:00 +0200

    updated location

--- buildInstallPamScript.sh
+++ buildInstallPamScript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd pam_script_module/libpam-script-0.1.12
+cd src/pam_script_module/libpam-script-0.1.12
 make clean
 make
 sudo cp pam_script.so /lib/security/
-cd  ../../
+cd  - >&2 2>/dev/null
